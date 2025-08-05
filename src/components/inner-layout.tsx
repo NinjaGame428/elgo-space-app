b2136e69dc03cdfa8be9385b79b5e29904177bb3
Josephine Phina Nzuku <nzukuphina24@gmail.com>
2025-08-05 22:18:01 +0000

    Remove the live chat

--- src/components/inner-layout.tsx
+++ src/components/inner-layout.tsx
@@ -1,14 +1,12 @@
 
 'use client';
 
-import { ChatWidget } from "./chat-widget";
 import { Footer } from "./footer";
 
 export function InnerLayout({ children }: { children: React.ReactNode; }) {
   return (
     <div className="flex-1 animate-fade-in-up">
         {children}
-        <ChatWidget />
         <Footer />
     </div>
   );
